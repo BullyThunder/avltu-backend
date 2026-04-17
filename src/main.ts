3dcd166c73c3f92a19c6b93eb9c482edc6177569
Bully_Thunder <vamosbarca2407@gmail.com>
2026-04-17 10:03:26 +0300

    save

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.FRONTEND_URL?.replace(/\/$/, ''),
+    origin: process.env.FRONTEND_URL,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
